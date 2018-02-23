e97f1a9ac69b04b2738c2bd48b74a99ca657cd42
BrezhnevSA <sbrezhnev@inbox.ru>
2018-02-23 19:36:14 +0300

    Fix

--- LAB1_/src/main/java/soapS/App.java
+++ LAB1_/src/main/java/soapS/App.java
@@ -3,7 +3,7 @@ package soapS;
 import javax.xml.ws.Endpoint;
 public class App {
     public static void main(String[] args) {
-        String url = "http://0.0.0.0:8080/PersonService";
+        String url = "http://0.0.0.0:8085/PersonService";
         Endpoint.publish(url, new PersonWebService());
     }
 }
--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 # labs
-
-LAB1 - stand-alone app for lab 1
-
-LAB1_ - java ee app for lab 1
-
-LAB1client - client's app for lab 1
